bca3d7639a0bae55f25c7696bade95894e28ee38
Davoleo <davoleo2001@gmail.com>
2019-03-16 20:52:01 +0100

    Buttons & button click handling
    + added gitIgnore

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,26 +1,57 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 /**
  * In JavaFX:
  * the Frame is called Stage
  * the Content is called Scene
  */
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+    private Button button, button2, button3;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("The Title!");
+
+        button = new Button("Click me!");
+        button.setOnAction(this);
+
+        button2 = new Button("Anonymous Inner class");
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                JOptionPane.showMessageDialog(null, "u clicked a button handled by an anonymous inner class");
+            }
+        });
+
+        button3 = new Button("Lambda Expression");
+        button3.setOnAction(event -> JOptionPane.showMessageDialog(null, "u clicked a button handled by a lambda expression"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        primaryStage.setScene(new Scene(layout, 300, 275));
         primaryStage.show();
     }
 
+    @Override
+    public void handle(ActionEvent event)
+    {
+        if (event.getSource() instanceof Button)
+            JOptionPane.showMessageDialog(null, "you clicked a button");
+
+    }
 
     public static void main(String[] args) {
         launch(args);
