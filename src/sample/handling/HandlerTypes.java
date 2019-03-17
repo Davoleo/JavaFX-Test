f1aa445847344b35e6402e25870bc1493b94e615
Davoleo <davoleo2001@gmail.com>
2019-03-17 23:37:39 +0100

    Switching scenes

---
+++ src/sample/handling/HandlerTypes.java
@@ -0,0 +1,62 @@
+package sample.handling;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 17/03/2019 / 23:24
+ * Class: HandlerTypes
+ * Project: JavaFX-Test
+ * Copyright - © - Davoleo - 2019
+ **************************************************/
+
+public class HandlerTypes extends Application implements EventHandler<ActionEvent> {
+
+    private Button button, button2, button3;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("The Title!");
+
+        //External Handler
+        button = new Button("Click me!");
+        button.setOnAction(this);
+
+        //Inner Class Handler
+        button2 = new Button("Anonymous Inner class");
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                JOptionPane.showMessageDialog(null, "u clicked a button handled by an anonymous inner class");
+            }
+        });
+
+        //Lambda expression handler
+        button3 = new Button("Lambda Expression");
+        button3.setOnAction(event -> JOptionPane.showMessageDialog(null, "u clicked a button handled by a lambda expression"));
+
+        VBox layout = new VBox(5);
+        layout.getChildren().addAll(button, button2, button3);
+
+        primaryStage.setScene(new Scene(layout, 300, 275));
+        primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event)
+    {
+        if (event.getSource() instanceof Button)
+            JOptionPane.showMessageDialog(null, "you clicked a button");
+    }
+}
---
+++ src/sample/scenes/SceneSwitcher.java
@@ -0,0 +1,48 @@
+package sample.scenes;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 17/03/2019 / 23:29
+ * Class: SceneSwitcher
+ * Project: JavaFX-Test
+ * Copyright - © - Davoleo - 2019
+ **************************************************/
+
+public class SceneSwitcher extends Application {
+
+    private Scene scene1, scene2;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Label label1 = new Label("Welcome the the first scene!");
+        Button button1 = new Button("Switch");
+        button1.setOnAction(event -> primaryStage.setScene(scene2));
+
+        //Layout 1 - VBox - vertical line
+        VBox layout1 = new VBox(3);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        //Button 2
+        Button button2 = new Button("Switch");
+        button2.setOnAction(event -> primaryStage.setScene(scene1));
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Scene Switcher");
+        primaryStage.show();
+    }
+}
