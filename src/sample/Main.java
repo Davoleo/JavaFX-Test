46782a9ff9f5421b3d851467141348ec9cc04776
Davoleo <davoleo2001@gmail.com>
2019-03-18 00:03:44 +0100

    SubWindows

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,11 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import sample.alertbox.SimpleAlertBox;
 
 /**
  * In JavaFX:
@@ -13,11 +14,20 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private Button button;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        button = new Button("AlertBox");
+        button.setOnAction(event -> SimpleAlertBox.display("SampleAlertBox", "WELCOME TO THE MALFORMED BOX!"));
+
+        VBox layout = new VBox(3);
+        layout.getChildren().add(button);
+
+        primaryStage.setTitle("Main JavaFX");
+        primaryStage.setScene(new Scene(layout, 300, 275));
         primaryStage.show();
     }
 
---
+++ src/sample/alertbox/SimpleAlertBox.java
@@ -0,0 +1,42 @@
+package sample.alertbox;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 17/03/2019 / 23:53
+ * Class: SimpleAlertBox
+ * Project: JavaFX-Test
+ * Copyright - © - Davoleo - 2019
+ **************************************************/
+
+public class SimpleAlertBox {
+
+    public static void display(String title, String message)
+    {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label(message);
+        Button button = new Button("Close");
+        button.setOnAction(event -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+}
