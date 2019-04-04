6e8a738194a278d80b83602278c14cc168678fa3
Davoleo <davoleo2001@gmail.com>
2019-04-04 20:25:09 +0200

    Embedded Layouts

---
+++ src/sample/layout/EmbeddedLayouts.java
@@ -0,0 +1,53 @@
+package sample.layout;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 04/04/2019 / 20:13
+ * Class: EmbeddedLayouts
+ * Project: JavaFX-Test
+ * Copyright - © - Davoleo - 2019
+ **************************************************/
+
+public class EmbeddedLayouts extends Application {
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("Embedded Layouts");
+
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+}
