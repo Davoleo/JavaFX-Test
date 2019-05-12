5c4ae2aa1fb806ff80f6b672e0e3200245eeb24e
Davoleo <davoleo2001@gmail.com>
2019-05-12 23:24:10 +0200

    Menus

---
+++ src/net/davoleo/javafxtest/control/MenuGui.java
@@ -0,0 +1,78 @@
+package net.davoleo.javafxtest.control;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 12/05/2019 / 22:55
+ * Class: MenuGui
+ * Project: JavaFX-Test
+ * Copyright - © - Davoleo - 2019
+ **************************************************/
+
+public class MenuGui extends Application {
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("Menus");
+
+        //File Menu
+        Menu fileMenu = new Menu("_File");
+
+        //File Menu Items
+        MenuItem newFile = new MenuItem("New...");
+        newFile.setOnAction(event -> System.out.println("Created a New File"));
+        MenuItem openFile = new MenuItem("Open...");
+        openFile.setOnAction(event -> System.out.println("Opened a File"));
+        MenuItem saveFile = new MenuItem("Save...");
+        saveFile.setOnAction(event -> System.out.println("Saved a File"));
+        MenuItem settings = new MenuItem("Settings");
+        settings.setOnAction(event -> System.out.println("Opened Settings"));
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(event -> primaryStage.close());
+
+        fileMenu.getItems().add(newFile);
+        fileMenu.getItems().add(openFile);
+        fileMenu.getItems().add(saveFile);
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(settings);
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(exit);
+
+        //Edit Menu
+        Menu editMenu = new Menu("_Edit");
+
+        MenuItem undo = new MenuItem("Undo");
+        undo.setDisable(true);
+
+        editMenu.getItems().addAll(new MenuItem("Cut"), new MenuItem("Copy"), new MenuItem("Paste"), undo);
+
+
+
+        //MenuBar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(fileMenu);
+        menuBar.getMenus().add(editMenu);
+
+        BorderPane layout = new BorderPane();
+        layout.setTop(menuBar);
+        Scene scene = new Scene(layout, 500, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+}
