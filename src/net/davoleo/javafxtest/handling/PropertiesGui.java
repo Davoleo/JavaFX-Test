ac4c21e921b7ebd44065e00320ae90442fa18d4c
Davoleo <davoleo2001@gmail.com>
2019-05-27 01:48:27 +0200

    Bound Controls

--- src/net/davoleo/javafxtest/handling/PropertiesGui.java
+++ src/net/davoleo/javafxtest/handling/PropertiesGui.java
@@ -3,15 +3,17 @@ package net.davoleo.javafxtest.handling;
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import net.davoleo.javafxtest.util.Person;
 
 public class PropertiesGui extends Application {
-
-    private Button button;
 
     public static void main(String[] args)
     {
@@ -24,6 +26,16 @@ public class PropertiesGui extends Application {
         primaryStage.setTitle("Properties");
 
         Person davoleo = new Person();
+        davoleo.firstNameProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Name changed to: " + newValue);
+            System.out.println("The Property object: " + davoleo.firstNameProperty());
+            System.out.println("The String value: " + davoleo.getFirstName());
+        });
+
+        Button button = new Button("Submit");
+        button.setOnAction(event -> davoleo.setFirstName("Tempest"));
+
+        //-----------------------------------------------------------
 
         IntegerProperty x = new SimpleIntegerProperty(3);
         IntegerProperty y = new SimpleIntegerProperty();
@@ -35,18 +47,21 @@ public class PropertiesGui extends Application {
         x.setValue(9);
         System.out.println(x.getValue() + " --- " + y.getValue());
 
-        davoleo.firstNameProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println("Name changed to: " + newValue);
-            System.out.println("The Property object: " + davoleo.firstNameProperty());
-            System.out.println("The String value: " + davoleo.getFirstName());
-        });
+        //-------------------------------------------------------
 
-        button = new Button("Submit");
-        button.setOnAction(event -> davoleo.setFirstName("Tempest"));
+        TextField input = new TextField();
+        input.setMaxWidth(200);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
+        Label welcomeLabel = new Label("Welcome to the Apocalypse ");
+        Label inputLabel = new Label();
+        inputLabel.textProperty().bind(input.textProperty());
+
+        HBox textLayout = new HBox(welcomeLabel, inputLabel);
+        textLayout.setAlignment(Pos.CENTER);
+
+        VBox layout = new VBox(10, input, textLayout, button);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout, 300, 150);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
